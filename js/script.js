c04d873f24a720defdfc3cb59eb23ed672c10d0f
Idedav <xdavide.correa@gmail.com>
2023-10-06 14:15:53 +0200

    [add] emails with api

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return{
+            emails:[],
+        }
+    },
+
+    methods:{
+        getApi(){
+            for(let i = 0; i < 10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((email)=>{
+                    this.emails.push(email.data.response)
+                })
+            }
+
+        }
+    },
+
+    mounted(){
+        this.getApi();
+        console.log(this.emails);
+    }
+}).mount('#app')
